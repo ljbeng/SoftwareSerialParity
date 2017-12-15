7667d4b0860369ab948026a8c95fbc3d20b1aa3e
Luke J. Barker <ljbwashburn@gmail.com>
2017-12-15 14:27:55 -0600

    Add files via upload

---
+++ SoftwareSerialParity/Examples/BasicUse/BasicUse.ino
@@ -0,0 +1,17 @@
+
+#include <SoftwareSerialParity.h>
+SoftwareSerialParity mySerial(2, 3); // RX, TX  (3, 2) for first pro mini boards 
+
+void setup() {
+  // put your setup code here, to run once:
+  mySerial.begin(9600,ODD);
+  //mySerial.begin(9600,EVEN);
+  //mySerial.begin(9600,NONE);
+   
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  mySerial.println("Hello Worlds");
+  delay(1000);
+}
